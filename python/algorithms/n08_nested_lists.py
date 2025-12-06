a9b99c6caa8c3c3d2c7da317aaae7c2b2093aad5
chuatt <dongda_2@yahoo.com>
2025-12-06 20:34:05 +0800

    Fixed n08_nested_list.py flake8-lint problem

--- python/algorithms/n08_nested_lists.py
+++ python/algorithms/n08_nested_lists.py
@@ -7,8 +7,8 @@ Link:
 https://www.hackerrank.com/challenges/nested-list/problem
 
 Summary:
-Given N students with their names and grades, print the name(s) of the student(s)
-having the second lowest grade.
+Given N students with their names and grades, print the name(s) of 
+the student(s) having the second lowest grade.
 
 If more than one student has the second lowest grade, print all such names in
 alphabetical order.
