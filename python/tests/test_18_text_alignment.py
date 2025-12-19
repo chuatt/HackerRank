8c6683224171bc2b5076cdeaa18b82c3e9b6ea93
chuatt <dongda_2@yahoo.com>
2025-12-19 17:25:05 +0800

    Update test_18_text_alignment.py to fix error

--- python/tests/test_18_text_alignment.py
+++ python/tests/test_18_text_alignment.py
@@ -2,9 +2,6 @@ from python.algorithms.n18_text_alignment import print_hackerrank_logo
 
 
 def test_text_alignment_thickness_3(capsys):
-    """
-    Test HackerRank logo output for thickness = 3.
-    """
     print_hackerrank_logo(3)
     captured = capsys.readouterr().out
 
@@ -12,32 +9,36 @@ def test_text_alignment_thickness_3(capsys):
         "  H  \n"
         " HHH \n"
         "HHHHH\n"
-        "HHH   HHH\n"
-        "HHH   HHH\n"
-        "HHHHHHHHH\n"
-        "HHH   HHH\n"
-        "HHH   HHH\n"
-        "  HHHHH  \n"
-        "    HHH  \n"
-        "      H  \n"
+        " HHH         HHH        \n"
+        " HHH         HHH        \n"
+        " HHH         HHH        \n"
+        " HHH         HHH        \n"
+        " HHHHHHHHHHHHHHH  \n"
+        " HHHHHHHHHHHHHHH  \n"
+        " HHH         HHH        \n"
+        " HHH         HHH        \n"
+        " HHH         HHH        \n"
+        " HHH         HHH        \n"
+        "            HHHHH \n"
+        "             HHH  \n"
+        "              H   \n"
     )
 
     assert captured == expected
 
 
 def test_text_alignment_thickness_1(capsys):
-    """
-    Edge case: smallest valid thickness.
-    """
     print_hackerrank_logo(1)
     captured = capsys.readouterr().out
 
     expected = (
         "H\n"
-        "H H\n"
-        "HHH\n"
-        "H H\n"
-        "H\n"
+        "H   H   \n"
+        "H   H   \n"
+        "HHHHH \n"
+        "H   H   \n"
+        "H   H   \n"
+        "    H \n"
     )
 
     assert captured == expected
